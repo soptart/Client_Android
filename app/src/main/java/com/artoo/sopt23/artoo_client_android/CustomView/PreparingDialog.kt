0891f5fa2fbb39a022fdda012ea619879e6256d5
dshyun0226 <dshyun0226@gmail.com>
2019-01-10 02:15:23 +0900

    ProductDetailModify + @

---
+++ app/src/main/java/com/artoo/sopt23/artoo_client_android/CustomView/PreparingDialog.kt
@@ -0,0 +1,25 @@
+package com.artoo.sopt23.artoo_client_android.CustomView
+
+import android.app.Dialog
+import android.content.Context
+import android.os.Bundle
+import android.view.Window
+import android.graphics.drawable.ColorDrawable
+import android.widget.TextView
+import com.artoo.sopt23.artoo_client_android.R
+import kotlinx.android.synthetic.main.dialog_preparingcard.*
+
+
+public class PreparingDialog(context: Context) : Dialog(context) {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        requestWindowFeature(Window.FEATURE_NO_TITLE)
+        setContentView(R.layout.dialog_preparingcard)
+        card_confirm_btn.setOnClickListener { dismiss() }
+    }
+
+
+
+
+}
